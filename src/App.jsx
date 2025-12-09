cb8158157cea8eadb7a02ed169b5ad76857b6768
Torkel0825 <dtk000825@gmail.com>
2025-12-09 12:57:34 +0100

    bop it!

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
-
+import "./components/styles/custom_specialty/fancyBorderWall.css";
 import Counter from "./components/counter.jsx"; // contains btn for increase and decrease
 import InputComponent from "./components/InputComponent.jsx";
-
 function App() {
   return (
     <>
       <Counter />
       <div style={{"padding-bottom": "3.2rem"}}></div>
+      <div class="fancyBorderWall"></div>
       <InputComponent />
     </>
   );
